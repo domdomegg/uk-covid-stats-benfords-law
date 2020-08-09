989c5680b893301d617e54e90c09f10a55edeeaa
Adam Jones <jones_adam@rocketmail.com>
2020-08-09 12:41:55 +0100

    Use CSV data
    Government CSV data is updated more frequently than the JSON data (every day, instead of every week)

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Data from [coronavirus.data.gov.uk](https://coronavirus.data.gov.uk/)
 
 ## Setup
 
-Install curl, Python and matplotlib
-
 ```
-curl -o data.json https://c19downloads.azureedge.net/downloads/json/coronavirus-cases_latest.json
+python -m venv env
+source env/bin/activate
+pip install -r requirements.txt
 ```
 
-## Running
+## Usage
 
 ```
 python analysis.py
--- analysis.py
+++ analysis.py
@@ -1,15 +1,32 @@
-import json
+import os
+import requests
+import csv
 import matplotlib.pyplot as plt
 
 buckets = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+if not os.path.isfile('data.csv'):
+    print('Downloading data...')
+    try:
+        data = requests.get('https://c19downloads.azureedge.net/downloads/csv/coronavirus-cases_latest.csv')
+    except Exception as e:
+        print('Failed to retrieve data:')
+        print(e)
+        exit(1)
+    with open('data.csv', 'w') as f:
+        f.write(data.text)
+
 # Parse and bucket the data
-with open('data.json') as f:
-    data = json.loads(f.read())
-    for ltla in data['ltlas']:
-        firstDigit = int(str(ltla['dailyLabConfirmedCases'])[0])
-        buckets[firstDigit] = buckets[firstDigit] + 1
-    lastUpdated = data['metadata']['lastUpdatedAt']
+print('Analysing data...')
+with open('data.csv') as f:
+    data = csv.reader(f)
+    lastUpdated = '0'
+    for row in data:
+        if row[2] == 'ltla':
+            firstDigit = int(str(row[4])[0])
+            buckets[firstDigit] = buckets[firstDigit] + 1
+        if row[3] != 'Specimen date':
+            lastUpdated = max(lastUpdated, row[3])
 
 # Ignore 0s
 buckets = buckets[1:]
@@ -20,9 +37,13 @@ for i in range(0, len(buckets)):
     buckets[i] = 100 * buckets[i] / total
 
 # Plot
+print('Plotting data...')
 fig, ax = plt.subplots()
 ax.plot(range(1, 10), buckets, label = 'COVID stats')
 ax.plot(range(1, 10), [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6], label = 'Benford\'s law')
 ax.set(xlabel = 'First digit', ylabel = 'Frequency (%)', title = 'UK COVID statistics follow Benford\'s law (as of ' + lastUpdated[0:10] + ')')
 plt.legend()
-plt.show()
+if os.environ.get('CI') == 'true' or os.environ.get('HEADLESS') == 'true':
+    plt.savefig('output.png')
+else:
+    plt.show()
